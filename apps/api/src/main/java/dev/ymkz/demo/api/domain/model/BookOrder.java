15a8c82a45fe21a8a6b5e4f6b92e39d1b573d289
ymkz <ymkz@users.noreply.github.com>
2025-09-13 23:36:14 +0900

    fix

--- apps/api/src/main/java/dev/ymkz/demo/api/domain/model/BookOrder.java
+++ apps/api/src/main/java/dev/ymkz/demo/api/domain/model/BookOrder.java
@@ -7,19 +7,14 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum BookOrder {
-
     @JsonProperty("+price")
     PRICE_ASC("+price"), // 価格の昇順
-
     @JsonProperty("-price")
     PRICE_DESC("-price"), // 価格の降順
-
     @JsonProperty("+published_at")
     PUBLISHED_AT_ASC("+published_at"), // 出版日の昇順
-
     @JsonProperty("-published_at")
-    PUBLISHED_AT_DESC("-published_at") // 出版日の降順
-;
+    PUBLISHED_AT_DESC("-published_at"); // 出版日の降順
 
     private final String value;
 
