9fcd2cb0eba9f3133cbc44e6baf5b88a71b9eec6
ymkz <ymkz@users.noreply.github.com>
2025-09-15 02:00:51 +0900

    wip

--- apps/api/src/intTest/java/dev/ymkz/demo/api/SearchBooksIntTest.java
+++ apps/api/src/intTest/java/dev/ymkz/demo/api/SearchBooksIntTest.java
@@ -1,10 +1,15 @@
 package dev.ymkz.demo.api;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -13,20 +18,32 @@ import org.testcontainers.containers.MySQLContainer;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SearchBooksIntTest {
 
+    @LocalServerPort
+    private Integer port;
+
     @TestConfiguration(proxyBeanMethods = false)
     @Import(DemoApiApplication.class)
     static class TestConfig {
         @Bean
         @ServiceConnection
-        MySQLContainer<?> mysqlContainer() {
+        MySQLContainer<?> mysql() {
             return new MySQLContainer<>("mysql:8");
         }
     }
 
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost:" + port;
+    }
+
     @Test
-    void shouldGetAllBookmarks() {
-        var actual = true;
-        var expected = false;
-        assertThat(actual).isEqualTo(expected);
+    void searchBooksが200でレスポンスされること() {
+        given()
+            .contentType(ContentType.JSON)
+            .when()
+            .get("/books")
+            .then()
+            .statusCode(200)
+            .body(".", hasSize(2));
     }
 }
