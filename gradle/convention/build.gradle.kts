cece6f6d4029febfda6a9ba54c555a54212aeedb
ymkz <ymkz@users.noreply.github.com>
2025-09-14 19:50:14 +0900

    spotlessをversion catalogで管理する

--- gradle/convention/build.gradle.kts
+++ gradle/convention/build.gradle.kts
@@ -6,8 +6,6 @@ repositories {
     mavenCentral()
 }
 
-val spotlessVersion = "7.2.1" // https://mvnrepository.com/artifact/com.diffplug.spotless/spotless-plugin-gradle
-
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:$spotlessVersion")
+    implementation(libs.spotless)
 }
