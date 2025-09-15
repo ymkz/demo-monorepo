c356f53d20d0849d7989fe580c613fac79089245
Kazuya Yamashita <ymkz@users.noreply.github.com>
2025-09-16 01:03:49 +0900

    intTest実装 (#180)
    * wip
    
    * wip
    
    * wip
    
    * intTestを動くようにする
    
    * 最終行の改行を統一

---
+++ apps/api/src/intTest/java/dev/ymkz/demo/api/SearchBooksIntTest.java
@@ -0,0 +1,33 @@
+package dev.ymkz.demo.api;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestContainersConfig.class)
+public class SearchBooksIntTest {
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    void searchBooksが200でレスポンスされること() {
+        given().contentType(ContentType.JSON)
+                .when()
+                .get("/books")
+                .then()
+                .statusCode(200);
+    }
+}
